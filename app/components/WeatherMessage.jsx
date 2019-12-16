342e7f00d30cfb3fa56a0664b8d5c64699d48a03
Brian Groat <brian@barnum.agency>
2019-12-16 15:27:38 -0500

    Completed Section 5 of Udemy course

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,17 +1,8 @@
 var React = require('react');
 
-// var WeatherMessage = React.createClass({
-//     render: function () {
-//         var {temp, location} = this.props;
-//         return (
-//             <h3>It is currently {temp} degrees in {location}</h3>
-//         );
-//     }
-// });
-
-var WeatherMessage = (props) => {
+var WeatherMessage = ({temp, location}) => {
     return (
-        <h3>It is currently {props.temp} degrees in {props.location} and it feels good</h3>
+        <h3>It is currently {temp} degrees in {location} and it feels good</h3>
     )
 }
 
