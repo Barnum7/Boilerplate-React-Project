f2ac3e333db3fee35fb895fb78e324a4a4499084
Brian Groat <brian@barnum.agency>
2019-12-23 12:30:09 -0500

    Added styling to weather page and components

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -31,14 +31,14 @@ var Weather = React.createClass({
 
         function renderMessage () {
             if (isLoading) {
-                return <h3>Fetching weather...</h3>;
+                return <h3 className="text-centre">Fetching weather...</h3>;
             } else if (temp && location) {
                 return <WeatherMessage temp={temp} location={location}/>
             }
         };
         return (
             <div>
-                <h3>Weather Component</h3>
+                <h1 className="text-centre">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch} />
                 {renderMessage()}
             </div>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
     return (
-        <h3>It is currently {temp} degrees in {location} and it feels good</h3>
+        <h3 className="text-centre">It is currently {temp} degrees in {location} and it feels good</h3>
     )
 }
 
