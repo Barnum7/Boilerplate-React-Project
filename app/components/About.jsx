ff92cc4b8cfa4c77806a9fd488adc3dc96473b7f
Brian Groat <brian@barnum.agency>
2020-01-03 08:35:25 -0500

    Added custom styline to components

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-        <h1 className="text-centre">About</h1>
+        <h1 className="text-center page-title">About</h1>
         <p>This is my Weather App built with <a href="https://facebook.github.io/react">React.JS</a> and using data from <a href="https://openweathermap.org">the Open Weather Map API</a></p>
     </div>
     );
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -37,7 +37,7 @@ var Weather = React.createClass({
 
         function renderMessage () {
             if (isLoading) {
-                return <h3 className="text-centre">Fetching weather...</h3>;
+                return <h3 className="text-center">Fetching weather...</h3>;
             } else if (temp && location) {
                 return <WeatherMessage temp={temp} location={location}/>
             }
@@ -53,7 +53,7 @@ var Weather = React.createClass({
 
         return (
             <div>
-                <h1 className="text-centre">Get Weather</h1>
+                <h1 className="page-title text-center">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch} />
                 {renderMessage()}
                 {renderError()}
